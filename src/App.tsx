4936412ce72687dfa8c555d558291256aea93def
Obaidullah Azad <azadobaidullah19@gmail.com>
2024-10-08 11:42:32 +0530

    Added pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignOut";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
 
 export default function App() {
-  return <div className="text-red-900">dasdjkashdjk</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
