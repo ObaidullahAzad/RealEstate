9bdc8c7506c9fcb72213f9686d0a346750ecd73d
Obaidullah Azad <azadobaidullah19@gmail.com>
2024-12-16 01:57:27 +0530

    added delete user fuction on button

--- frontend/src/redux/user/userSlice.ts
+++ frontend/src/redux/user/userSlice.ts
@@ -40,6 +40,18 @@ export const userSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
+    deleteUserStart: (state) => {
+      state.loading = true;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
   },
 });
 
@@ -50,5 +62,8 @@ export const {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
 } = userSlice.actions;
 export default userSlice.reducer;
