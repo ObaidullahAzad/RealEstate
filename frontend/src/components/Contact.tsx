21996205470b6efe7093ad59cc8e344cffe79723
Obaidullah Azad <azadobaidullah19@gmail.com>
2024-12-19 19:18:04 +0530

    search api

--- frontend/src/components/Contact.tsx
+++ frontend/src/components/Contact.tsx
@@ -26,7 +26,7 @@ export default function Contact({ listing }: any) {
       {landlord && (
         <div className="flex flex-col gap-2">
           <p>
-            Contact <span className="font-semibold">{landlord.username}</span>{" "}
+            Contact <span className="font-semibold">{landlord.username}</span>
             for -
             <span className="font-semibold">{listing.name.toLowerCase()}</span>
           </p>
