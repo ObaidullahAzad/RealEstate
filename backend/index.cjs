71123fe1e15a9859044eb487596ace2953903fbc
Obaidullah Azad <azadobaidullah19@gmail.com>
2024-12-20 10:34:38 +0530

    port

--- backend/index.cjs
+++ backend/index.cjs
@@ -23,7 +23,7 @@ app.use(cookieParser());
 
 app.use(express.json());
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000!!!!");
 });
 
