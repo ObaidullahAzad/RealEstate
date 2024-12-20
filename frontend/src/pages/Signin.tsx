e711ae2ce488e63f8981ffddc1f028a13510d71d
Obaidullah Azad <azadobaidullah19@gmail.com>
2024-12-20 17:06:29 +0530

    update signin API endpoint to use production URL

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -23,13 +23,16 @@ export default function Signin() {
     e.preventDefault();
     try {
       dispatch(signInStart());
-      const res = await fetch("/api/auth/signin", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+      const res = await fetch(
+        "https://realestate-o4z2.onrender.com/api/auth/signin",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
       const data = await res.json();
       if (data.success === false) {
         dispatch(signInFailue(data.message));
